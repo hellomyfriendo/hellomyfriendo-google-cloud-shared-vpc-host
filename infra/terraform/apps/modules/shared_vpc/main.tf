aa41a63e44533b54fd36405afcfa6976f3b35af6
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-09-19 21:28:14 -0400

    Fix typo in subnet creation

--- infra/terraform/apps/modules/shared_vpc/main.tf
+++ infra/terraform/apps/modules/shared_vpc/main.tf
@@ -13,20 +13,10 @@ module "vpc" {
 
   subnets = [
     {
-      subnet_name = "hmf-na-ne-1-${var.environment}-api-subnet-1"
-      # TODO(Marcus): Learn how to and plan the IP ranges.
-      subnet_ip              = "10.128.0.0/16"
-      subnet_region          = "northamerica-northeast1"
-      subnets_private_access = "true"
-      # TODO(Marcus): Enable this when I can pay for it.
-      # subnet_flow_logs          = true
-      # subnet_flow_logs_sampling = "1.0"
-    },
-    {
-      subnet_name            = "na-ne-1-${var.environment}-vpcaccess-conn-1"
-      subnet_ip              = "10.129.0.0/28"
-      subnet_region          = "northamerica-northeast1"
-      subnets_private_access = "true"
+      subnet_name           = "na-ne-1-${var.environment}-vpcaccess-conn-1"
+      subnet_ip             = "10.129.0.0/28"
+      subnet_region         = "northamerica-northeast1"
+      subnet_private_access = "true"
     }
   ]
 
@@ -36,11 +26,10 @@ module "vpc" {
 }
 
 module "private_service_access" {
-  source        = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
-  version       = "~> 13.0"
-  project_id    = data.google_project.project.project_id
-  vpc_network   = module.vpc.network_name
-  prefix_length = 20
+  source      = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
+  version     = "~> 13.0"
+  project_id  = data.google_project.project.project_id
+  vpc_network = module.vpc.network_name
 }
 
 # TODO(Marcus): Raise these values when I can pay for it.
